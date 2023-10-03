d8c76947afc29fe1a70978e5c129a8e02e5e6f61
mishrasidhant <31853317+mishrasidhant@users.noreply.github.com>
2023-10-02 22:48:00 -0400

    Move slider into row-container

--- src/features/movie/titleDisplay/titleDisplay.js
+++ src/features/movie/titleDisplay/titleDisplay.js
@@ -1,13 +1,13 @@
-import {Slider} from '../../movie';
-import './titleDisplay.css';
+import { Slider } from "../../movie";
+import "./titleDisplay.css";
 
-export default function TitleDisplayRow(){
-	return (
-      <div className="title-display-row">
-        <h2 className="row-header">Row Header</h2>
-        <Slider></Slider>
-        <div className="row-container">
-        </div>
+export default function TitleDisplayRow() {
+  return (
+    <div className="title-display-row">
+      <h2 className="row-header">Row Header</h2>
+      <div className="row-container">
+        <Slider />
       </div>
-	)
+    </div>
+  );
 }
