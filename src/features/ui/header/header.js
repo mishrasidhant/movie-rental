d262d5e336db49c845a4fd1b2ba0128344cffaf1
mishrasidhant <31853317+mishrasidhant@users.noreply.github.com>
2023-10-09 11:02:01 -0400

    Use element selector instead of class name

--- src/features/ui/header/header.js
+++ src/features/ui/header/header.js
@@ -5,23 +5,23 @@ import "./header.css";
 export default function Header() {
   return (
     <div className="header-container">
-    <a href="#default" className="logo">
-    MovieRental
-    </a>
-    <ul className="primary-navigation">
-      <li className="navigation-tab">
-        <a href="#default">Home</a>
-      </li>
-      <li className="navigation-tab">
-        <a href="#default">Movies</a>
-      </li>
-      <li className="navigation-tab">
-        <a href="#default">My List</a>
-      </li>
-    </ul>
-    <div className="secondary-navigation">
-      <Placeholder placeholderText="Secondary Navigation Menu"></Placeholder>
-    </div>
+      <a href="#default" className="logo">
+        MovieRental
+      </a>
+      <ul>
+        <li>
+          <a href="#default">Home</a>
+        </li>
+        <li>
+          <a href="#default">Movies</a>
+        </li>
+        <li>
+          <a href="#default">My List</a>
+        </li>
+      </ul>
+      <div className="secondary-navigation">
+        <Placeholder placeholderText="Secondary Navigation Menu"></Placeholder>
+      </div>
     </div>
   );
 }
