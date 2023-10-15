73cda6f985e8ecc58891bf898204686691150871
mishrasidhant <31853317+mishrasidhant@users.noreply.github.com>
2023-10-15 09:43:56 -0400

    Add search button - ffs this was rough

--- src/features/ui/header/header.js
+++ src/features/ui/header/header.js
@@ -1,4 +1,4 @@
-import { Placeholder } from "../../ui";
+import { CustomButton, Placeholder } from "../../ui";
 import logo from "../../../assets/logo-cineema-myke-simon-small.jpg?width=100";
 import "./header.css";
 
@@ -25,7 +25,26 @@ export default function Header() {
         </li>
       </ul>
       <div className="secondary-navigation">
-        <Placeholder placeholderText="Secondary Navigation Menu"></Placeholder>
+      <div className="nav-element">
+        <div className="search-box">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="search-icon"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+
+      </div>
+        {/* <Placeholder placeholderText="Secondary Navigation Menu"></Placeholder> */}
       </div>
     </div>
   );
